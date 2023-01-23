73fe3e0fdde9ebdc5587c7a6937b06043d5f8263
evgenij <samitin.e@gmail.com>
2023-01-23 23:42:14 +1100

    test 1.3 entities

---
+++ app/src/main/java/ru/samitin/cleantest/postexample/entities/User.kt
@@ -0,0 +1,9 @@
+package ru.samitin.cleantest.postexample.entities
+
+data class User(
+    private val name : String,
+    private val username : String,
+    private val email : String,
+    private val phone : String,
+    private val city : String
+)
