f3939565379ff0575c16de54bce3de019b1c7e57
evgenij <samitin.e@gmail.com>
2023-01-23 15:26:53 +1100

    test 1.2

--- app/src/main/java/ru/samitin/cleantest/postexample/use_cases/UseCaseInteractor.kt
+++ app/src/main/java/ru/samitin/cleantest/postexample/use_cases/UseCaseInteractor.kt
@@ -1,12 +1,12 @@
 package ru.samitin.cleantest.postexample.use_cases
 
-class UseCaseInteractor(private val outputPort: UseCaseOutputPort) : UseCaseInputPort {
+class UseCaseInteractor(private val useCaseOutputPort: UseCaseOutputPort) : UseCaseInputPort {
 
     override fun processInput(inputData: String) {
-        val outputData = showOutput(inputData)
-        outputPort.showOutput(outputData)
+        val outputData = process(inputData)
+        useCaseOutputPort.showOutput(outputData)
     }
-    fun showOutput(outputData: String):String{
+    fun process(outputData: String):String{
         return outputData
     }
 }
